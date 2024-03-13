dbe6420cda7eadc0bc4d2eb4d8df18b53cdc3c38
ahmad <ahmadroshanfar@gmail.com>
2024-03-13 14:49:04 +0330

    web app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createEffect, onMount } from "solid-js";
+import styles from "./App.module.css";
+import logo from "./logo.svg";
+
+const telegram = window.Telegram.WebApp;
+function App() {
+  const initTelegram = () => {
+    telegram.ready();
+    // telegram.SettingsButton.show();
+    // telegram.BackButton.show();
+    telegram.MainButton.text = "Pay :)";
+    telegram.MainButton.show();
+    telegram.MainButton.onClick(telegram.sendData("HELLO"));
+  };
+
+  onMount(() => {
+    // telegramRef.current = document.querySelector("#main_page");
+    createEffect(initTelegram);
+  });
+
+  return <div>Hello</div>;
+}
+
+export default App;
