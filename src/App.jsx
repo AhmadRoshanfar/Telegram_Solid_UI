95d723c50896f4a3f8e2dd133820e5e56946b782
ahmad <ahmadroshanfar@gmail.com>
2024-03-13 19:26:24 +0330

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import { createEffect, onMount } from "solid-js";
 import styles from "./App.module.css";
 import logo from "./logo.svg";
 
-const telegram = window.Telegram.WebApp;
+// const telegram = window.Telegram.WebApp;
 function App() {
-  const initTelegram = () => {
-    telegram.ready();
-    // telegram.SettingsButton.show();
-    // telegram.BackButton.show();
-    telegram.MainButton.text = "Pay :)";
-    telegram.MainButton.show();
-    telegram.MainButton.onClick(telegram.sendData("HELLO"));
-  };
+  // const initTelegram = () => {
+  //   telegram.ready();
+  //   // telegram.SettingsButton.show();
+  //   // telegram.BackButton.show();
+  //   telegram.MainButton.text = "Pay :)";
+  //   telegram.MainButton.show();
+  //   telegram.MainButton.onClick(telegram.sendData("HELLO"));
+  // };
 
-  onMount(() => {
-    // telegramRef.current = document.querySelector("#main_page");
-    createEffect(initTelegram);
-  });
+  // onMount(() => {
+  //   // telegramRef.current = document.querySelector("#main_page");
+  //   createEffect(initTelegram);
+  // });
 
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>HELLOOOO</h1>
+    </div>
+  );
 }
 
 export default App;
